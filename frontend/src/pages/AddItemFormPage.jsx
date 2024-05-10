588a29ddbc1b0df8f3dda0fbd0ce7fc01c003fca
NoZ915 <tina0915tw0915@gmail.com>
2024-05-10 21:51:11 +0800

    added: form's 2 select

--- frontend/src/pages/AddItemFormPage.jsx
+++ frontend/src/pages/AddItemFormPage.jsx
@@ -1,8 +1,92 @@
-function AddItemFormPage(){
-    return(
-        <>
-            Add Item
-        </>
+import { Container, MenuItem, TextField, Typography, Box } from "@mui/material";
+
+const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    margin: "0 auto",
+    mt: 4,
+    width: '80%',
+    borderRadius: 2,
+    boxShadow: 10,
+    pt: 2,
+    px: 4,
+    pb: 3,
+    backgroundColor: "white"
+}
+const selectBoxStyle = {
+    display: "flex",
+    alignItems: "baseline",
+}
+const selectTitleStyle = {
+    display: "inline-block",
+    mr: 2
+}
+
+const years = [
+    {
+        value: "111",
+        label: "111年"
+    },
+    {
+        value: "110",
+        label: "110年"
+    }
+];
+const formats = [
+    {
+        value: "fillIn",
+        label: "填充題"
+    },
+    {
+        value: "calculation",
+        label: "計算題"
+    }
+];
+const types = [
+    {
+        value: ""
+    }
+]
+
+function AddItemFormPage() {
+    return (
+        <Container sx={{ ...containerStyle }}>
+            <form>
+                {/* 年份 */}
+                <Box sx={{...selectBoxStyle}}>
+                    <Typography sx={{ ...selectTitleStyle }} component="h3">試題年份</Typography>
+                    <TextField
+                        id="year"
+                        select
+                        defaultValue="111"
+                        helperText="請選擇試題年份"
+                    >
+                        {years.map((year) => (
+                            <MenuItem key={year.value} value={year.value}>
+                                {year.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Box>
+
+                {/* 形式 */}
+                <Box sx={{...selectBoxStyle}}>
+                    <Typography sx={{ ...selectTitleStyle }} component="h3">試題形式</Typography>
+                    <TextField
+                        id="format"
+                        select
+                        defaultValue="fillIn"
+                        helperText="請選擇試題形式"
+                    >
+                        {formats.map((format) => (
+                            <MenuItem key={format.value} value={format.value}>
+                                {format.label}
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </Box>
+            </form>
+        </Container>
     )
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,147 @@
-function HomePage(){
-    return(
-        <div>Home</div>
+import { Box, Container, Typography } from "@mui/material";
+
+const dynamicTextStyle1 = {
+    position: "relative",
+    whiteSpace: "nowrap",
+    "@keyframes typewriter1": {
+        "30%, 50%": {
+            left: "75%"
+        },
+        //40%~60% 讓字固定在最長的長度
+        "80%": {
+            left: "0"
+        }
+        //100% 再讓所有字消失，好像有人在把字刪掉的感覺～
+    },
+    "@keyframes blink": {
+        from: { borderLeft: "solid 1px  #EDDE5D" },
+        to: { borderLeft: "solid 1px  transparent" }
+    },
+    '&::before': {
+        content: "''",
+        position: "absolute",
+        borderLeft: " solid 1px  #EDDE5D",
+        width: "100%",
+        height: "100%",
+        bottom: "0",
+        left: "0",
+        background: "#fb8c00",
+        animation: `typewriter1 4s steps(23) infinite, blink 0.5s steps(23) infinite`
+    }
+}
+const dynamicTextStyle2 = {
+    position: "relative",
+    whiteSpace: "nowrap",
+    "@keyframes typewriter2": {
+        "50%, 70%": {
+            left: "88%"
+        },
+        //40%~60% 讓字固定在最長的長度
+        "90%": {
+            left: "0"
+        }
+        //100% 再讓所有字消失，好像有人在把字刪掉的感覺～
+    },
+    "@keyframes blink": {
+        from: { borderLeft: "solid 1px  #EDDE5D" },
+        to: { borderLeft: "solid 1px  transparent" }
+    },
+    '&::before': {
+        content: "''",
+        position: "absolute",
+        borderLeft: " solid 1px  #EDDE5D",
+        width: "100%",
+        height: "100%",
+        bottom: "0",
+        left: "0",
+        background: "#fb8c00",
+        animation: `typewriter2 4s steps(23) infinite, blink 0.5s steps(23) infinite`
+    }
+}
+const dynamicTextStyle3 = {
+    position: "relative",
+    whiteSpace: "nowrap",
+    "@keyframes typewriter3": {
+        "60%, 80%": {
+            left: "85%"
+        },
+        //40%~60% 讓字固定在最長的長度
+        "100%": {
+            left: "0"
+        }
+        //100% 再讓所有字消失，好像有人在把字刪掉的感覺～
+    },
+    "@keyframes blink": {
+        from: { borderLeft: "solid 1px  #EDDE5D" },
+        to: { borderLeft: "solid 1px  transparent" }
+    },
+    '&::before': {
+        content: "''",
+        position: "absolute",
+        borderLeft: " solid 1px  #EDDE5D",
+        width: "100%",
+        height: "100%",
+        bottom: "0",
+        left: "0",
+        background: "#fb8c00",
+        animation: `typewriter3 4s steps(23) infinite, blink 0.5s steps(23) infinite`
+    }
+}
+
+function HomePage() {
+    return (
+        <Box
+            sx={{
+                backgroundColor: "#fb8c00",
+                height: "100%"
+            }}
+        >
+            <Container sx={{
+                paddingTop: "20px",
+                display: "flex",
+                flexDirection: "column",
+                height: "inherit",
+                justifyContent: "center",
+                width: "900px",
+                overflow: "hidden",
+                position: "relative"
+            }}>
+                <Typography
+                    sx={{
+                        ...dynamicTextStyle1,
+                        color: "white",
+                        fontSize: "6em"
+                    }}
+                    variant="h1"
+                    gutterBottom
+                >
+                    GENETIC Algo
+                </Typography>
+                <Typography
+                    sx={{
+                        ...dynamicTextStyle2,
+                        color: "white",
+                        fontSize: "6em"
+                    }}
+                    variant="h1"
+                    gutterBottom
+                >
+                    PARALLEL TEST
+                </Typography>
+                <Typography
+                    sx={{
+                        ...dynamicTextStyle3,
+                        color: "white",
+                        fontSize: "6em"
+                    }}
+                    variant="h1"
+                    gutterBottom
+                >
+                    ITEM SELETION
+                </Typography>
+            </Container>
+
+        </Box >
     )
 }
 
